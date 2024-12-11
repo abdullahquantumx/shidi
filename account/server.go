1e053315bef85dd069e699a3b9c26986b4716fba
Shridhar2104 <155125695+Shridhar2104@users.noreply.github.com>
2024-12-11 12:23:32 +0530

    dummy

--- account/server.go
+++ account/server.go
@@ -14,12 +14,14 @@ import (
 type grpcServer struct {
 	pb.UnimplementedAccountServiceServer
 	service Service
+	
 }
 
 func NewGRPCServer(service Service, port int) error {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		return err
+
 	}
 
 	server := grpc.NewServer()
